717583be4f725adeb17a7929fe417f082abd447a
ktoodo <kristjan.toodo@gmail.com>
2021-12-20 11:20:15 +0200

    Starting refactoring to include SMP algo

--- lib/main.rb
+++ lib/main.rb
@@ -6,52 +6,53 @@ module Marriage
 end
 
 class Elves
-  attr_accessor :name, :married, :married_to, :proposed_to
+  attr_accessor :name, :married, :married_to, :proposed_to, :preferences
   include Marriage
 
-  def initialize(name, proposed_to = [])
+  def initialize(name, proposed_to = [], preferences = {})
     @name = name
     @married = false
     @proposed_to = proposed_to
+    @preferences = preferences
   end
 
-  def propose_to(dwarf_name, score, edm)
-    @proposed_to.push(dwarf_name) #remember all dwarves to whom the elf has proposed to
-    #marry if both parties are unmarried
-    if Edm.dwarves[dwarf_name].married == false && @married == false
-      marry_to(dwarf_name, score)
-      Edm.dwarves[dwarf_name].accept_proposal(@name, score)
-    #check if married elf would like new unmarried dwarf more
-    #if true / marry new and unmarry old
-    elsif Edm.dwarves[dwarf_name].married == false && @married == true
-      if (score[0] > self.bond[0])# && (score[1] > self.bond[1])
-        edm.set(Dwarves, @married_to)
-        marry_to(dwarf_name, score)
-        Edm.dwarves[dwarf_name].accept_proposal(self.name, score)
-      end
-    #check if married dwarf would like new unmarried elf more
-    #if true / marry new and unmarry old
-    elsif Edm.dwarves[dwarf_name].married == true && @married == false
-      previous_proposal_by = Edm.dwarves[dwarf_name].married_to
-      previous_proposal_score = Edm.dwarves[dwarf_name].bond
-      if (score[1] > previous_proposal_score[1])
-        marry_to(dwarf_name, score)
-        edm.set(Elves, previous_proposal_by, Edm.elves[previous_proposal_by].proposed_to)
-        Edm.dwarves[dwarf_name].accept_proposal(self.name, score)
-      end
-    #check if both married partners would like eachother more than their previous partners
-    elsif Edm.dwarves[dwarf_name].married == true && @married == true
-      previous_proposal_by = Edm.dwarves[dwarf_name].married_to
-      previous_proposal_score = Edm.dwarves[dwarf_name].bond
-      #check if elf would like the new one more and if the dwarf would like a new partner more than the previous
-      if ((score[0] > self.bond[0]) && (score[1] > previous_proposal_score[1]))
-        edm.set(Dwarves, @married_to)
-        marry_to(dwarf_name, score)
-        edm.set(Elves, previous_proposal_by, Edm.elves[previous_proposal_by].proposed_to)
-        Edm.dwarves[dwarf_name].accept_proposal(self.name, score)
-      end
-    end
-  end
+  # def propose_to(dwarf_name, score, edm)
+  #   @proposed_to.push(dwarf_name) #remember all dwarves to whom the elf has proposed to
+  #   #marry if both parties are unmarried
+  #   if Edm.dwarves[dwarf_name].married == false && @married == false
+  #     marry_to(dwarf_name, score)
+  #     Edm.dwarves[dwarf_name].accept_proposal(@name, score)
+  #   #check if married elf would like new unmarried dwarf more
+  #   #if true / marry new and unmarry old
+  #   elsif Edm.dwarves[dwarf_name].married == false && @married == true
+  #     if (score[0] > self.bond[0])# && (score[1] > self.bond[1])
+  #       edm.set(Dwarves, @married_to)
+  #       marry_to(dwarf_name, score)
+  #       Edm.dwarves[dwarf_name].accept_proposal(self.name, score)
+  #     end
+  #   #check if married dwarf would like new unmarried elf more
+  #   #if true / marry new and unmarry old
+  #   elsif Edm.dwarves[dwarf_name].married == true && @married == false
+  #     previous_proposal_by = Edm.dwarves[dwarf_name].married_to
+  #     previous_proposal_score = Edm.dwarves[dwarf_name].bond
+  #     if (score[1] > previous_proposal_score[1])
+  #       marry_to(dwarf_name, score)
+  #       edm.set(Elves, previous_proposal_by, Edm.elves[previous_proposal_by].proposed_to)
+  #       Edm.dwarves[dwarf_name].accept_proposal(self.name, score)
+  #     end
+  #   #check if both married partners would like eachother more than their previous partners
+  #   elsif Edm.dwarves[dwarf_name].married == true && @married == true
+  #     previous_proposal_by = Edm.dwarves[dwarf_name].married_to
+  #     previous_proposal_score = Edm.dwarves[dwarf_name].bond
+  #     #check if elf would like the new one more and if the dwarf would like a new partner more than the previous
+  #     if ((score[0] > self.bond[0]) && (score[1] > previous_proposal_score[1]))
+  #       edm.set(Dwarves, @married_to)
+  #       marry_to(dwarf_name, score)
+  #       edm.set(Elves, previous_proposal_by, Edm.elves[previous_proposal_by].proposed_to)
+  #       Edm.dwarves[dwarf_name].accept_proposal(self.name, score)
+  #     end
+  #   end
+  # end
 
   def marry_to(name, score)
     @married = true
@@ -62,12 +63,13 @@ class Elves
 end
 
 class Dwarves
-  attr_reader :name, :married, :married_to
+  attr_reader :name, :married, :married_to, :preferences
   include Marriage
 
-  def initialize(name)
+  def initialize(name, preferences = {})
     @name = name
     @married = false
+    @preferences = {}
   end
 
   def consider_proposal(swings, swing_type)
@@ -86,7 +88,7 @@ class Edm
   @@dwarves = {}
 
   def initialize
-    import_data('test2') #select file to read in
+    import_data('test1') #select file to read in
   end
 
   def set(race, name, proposed_to = [])
@@ -120,18 +122,19 @@ class Edm
         line = line.gsub(/,/, '').split()
         line -= ['Dwarves:']
         line.each {|dwarf| set(Dwarves, dwarf)}
-      #analyze encounters till end of document
+      #analyze encounters and set preferences till end of document
       else
         line = line.gsub(/[:=x]/, "").squeeze(" ").split
         elf = line[0]
         dwarf = line[1]
         score = []
-        score << score_translate(line[2])
-        score << score_translate(line[3])
-        @@elves[elf].propose_to(dwarf, score, self)
+        @@elves[elf].preferences[dwarf] = score_translate(line[2])
+        @@dwarves[dwarf].preferences[elf] = score_translate(line[3])
+        #score << score_translate(line[2])
+        #score << score_translate(line[3])
+        #@@elves[elf].propose_to(dwarf, score, self)
       end
     end
-    print_result
   end
 
   def score_translate(score)
